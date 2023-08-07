0904e6c22e017c8e93c66c72f39e5aa966b44bbd
sgerodes <sgerodes@gmail.com>
2023-08-07 18:57:06 +0200

    delete unnecessary modifier

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,39 +4,16 @@ pragma solidity 0.8.21;
 
 contract GasContract {
 
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-    
-    /*
-    struct Payment {
-        bytes8 recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 paymentID;
-        uint256 amount;
-        PaymentType paymentType;
-    }*/
-
     uint8 private constant MAX_ADMINS = 5;
     address private immutable contractOwner;
-    //uint256 private immutable totalSupply; // cannot be updated
     uint256 private paymentCounter = 0;
 
-    //mapping(uint256 => Payment) private payments;  // paymentCounter -> Payment 
     mapping(address => uint256) private whiteListStruct; 
     mapping(uint256 => address) public administrators;
     mapping(address => uint256) public whitelist;
     mapping(address => uint256) public balances;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
-    //event supplyChanged(address indexed, uint256 indexed);
-    //event Transfer(address recipient, uint256 amount);
-    //event PaymentUpdated(address admin, uint256 ID, uint256 amount, bytes8 recipient);
     event WhiteListTransfer(address indexed);
     
 
@@ -45,18 +22,10 @@ contract GasContract {
         _;
     }
 
-    modifier checkIfWhiteListed(address sender) {
-        //require(whitelist[msg.sender] != 0);
-        if (whitelist[msg.sender] == 0) revert();
-        _;
-    }
-
     function internalOnlyAdminOrOwner() internal {
         //if (!checkForAdmin(msg.sender) && msg.sender != contractOwner) revert();
         require(checkForAdmin(msg.sender) || msg.sender == contractOwner);
     }
-
-
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
@@ -65,42 +34,22 @@ contract GasContract {
                 administrators[ii] = _admins[ii];
                 if (_admins[ii] == msg.sender) {
                     balances[msg.sender] = _totalSupply;
-                    //emit supplyChanged(_admins[ii], _totalSupply);
                 }
                 unchecked {
-	                ++ii;
+                    ++
+                    ii
+                    ;
 	            }
         }
     }
-
-    /*
-    function updatePayment(
-        address _user,
-        uint256 _ID,
-        uint256 _amount,
-        PaymentType _type
-    ) external onlyAdminOrOwner {
-        if (_amount <= 0) revert(); // Message deleted for gas savings. Add a small message like "Amount should be > 0" 
-        //assert(_amount > 0);
-
-        //Payment storage paymentToUpdate = payments[_ID];
-        //paymentToUpdate.admin = _user;
-        //paymentToUpdate.paymentType = _type;
-        //paymentToUpdate.amount = _amount;
-        //emit PaymentUpdated(msg.sender, _ID, _amount, paymentToUpdate.recipientName);
-    }
-    */
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
         public
         onlyAdminOrOwner
     {
-        //require(_tier < 255); // Message deleted for gas savings. Add a small message like "tier should be < 255"
-        if (_tier >= 255) revert(); // Message deleted for gas savings. Add a small message like "tier should be < 255"
-        //assert(_tier < 255);
-
+        //require(_tier < 255);
+        if (_tier >= 255) revert();
         whitelist[_userAddrs] = _tier > 3 ? 3 : _tier;
-
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
@@ -108,13 +57,7 @@ contract GasContract {
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public checkIfWhiteListed(msg.sender) {
-        
-        //require(balances[msg.sender] >= _amount); // Message deleted for gas savings. Add a small message like "insufficient Balanc"
-        //if (balances[msg.sender] < _amount) revert(); // Message deleted for gas savings. Add a small message like "insufficient Balanc"
-
-        //require(_amount > 3); // Message deleted for gas savings. Add a small message like "amount should be > 3"
-        //if (_amount <= 3) revert(); // Message deleted for gas savings. Add a small message like "amount should be > 3"
+    ) public {
 
         if (balances[msg.sender] < _amount || _amount <= 3) revert();
         unchecked {
@@ -134,7 +77,9 @@ contract GasContract {
                 admin = true;
             }
             unchecked {
-                ++ii;
+                ++
+                ii
+                ;
             }
         }
     }
@@ -156,7 +101,6 @@ contract GasContract {
             balances[msg.sender] -= _amount;
             balances[_recipient] += _amount;
         }
-        //emit Transfer(_recipient, _amount);
 
         bytes8 nameAsBytes8;
         assembly {
@@ -164,12 +108,6 @@ contract GasContract {
             calldatacopy(tempName, add(add(_name.offset, 0x20), 8), 8) 
             nameAsBytes8 := mload(tempName) 
         }
-
-        //payments[paymentCounter].paymentType = PaymentType.BasicPayment;
-        //payments[paymentCounter].recipient = _recipient;
-        //payments[paymentCounter].amount = _amount;
-        //payments[paymentCounter].recipientName = nameAsBytes8;  // __name is max 8 bytes
-        //payments[paymentCounter].paymentID = paymentCounter++;
     }
 
 
