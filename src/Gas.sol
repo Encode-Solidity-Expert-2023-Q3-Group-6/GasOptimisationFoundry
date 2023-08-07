c8cd2a87e04a474ee7a7b6547ee93c062e048318
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-08-07 20:05:49 +0530

    Update Contract

--- src/Gas.sol
+++ src/Gas.sol
@@ -3,58 +3,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-error Gas_OnlyAdminCanCall();
-error Gas_TierLevelGreaterThan255();
-error Gas_UserNotWhitelistes();
-error Gas_IncorrectUserTier();
-error Gas_InsufficientBalance();
-error Gas_RecipientNameTooLong();
-error Gas_InsufficientAmount();
+// error Gas_OnlyAdminCanCall();
+// error Gas_TierLevelGreaterThan255();
+// error Gas_UserNotWhitelistes();
+// error Gas_IncorrectUserTier();
+// error Gas_InsufficientBalance();
+// error Gas_RecipientNameTooLong();
+// error Gas_InsufficientAmount();
 
 contract GasContract {
     // Type Decalarations
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
 
     struct Payment {
         uint256 paymentID;
         uint256 amount;
-        bool adminUpdated;
-        PaymentType paymentType;
         address recipient;
-        address admin;
-        bytes recipientName; // changed to bytes
+        bytes8 recipientName; // changed to bytes
     } // Changed layout and type
 
     // State Variables
-    uint256 public constant TRADE_PERCENT = 12; // Changed to constant
-    uint256 public immutable totalSupply; // Removed Initialization & changed to immutable
-    uint256 public paymentCounter = 0;
+    uint256 private immutable totalSupply; // Removed Initialization & changed to immutable
+    uint256 private paymentCounter = 0;
 
     bool wasLastOdd = true;
-    address public contractOwner;
+    address private contractOwner;
     address[5] public administrators;
 
-    mapping(address => Payment[]) public payments;
+    mapping(address => Payment[]) private payments;
     mapping(address => uint256) public whitelist;
     mapping(address => uint256) public balances;
-    mapping(address => bool) public isOddWhitelistUser;
-    mapping(address => uint256) public whiteListStruct;
+    mapping(address => bool) private isOddWhitelistUser;
+    mapping(address => uint256) private whiteListStruct;
 
     // Events
     event AddedToWhitelist(address userAddress, uint256 tier);
-    event supplyChanged(address indexed, uint256);
-    event Transfer(address recipient, uint256 amount);
+    event supplyChanged(address indexed, uint256 indexed);
+    event Transfer(address indexed recipient, uint256 indexed amount);
     event PaymentUpdated(
-        address admin,
-        uint256 ID,
+        address indexed admin,
+        uint256 indexed ID,
         uint256 amount,
-        bytes recipient
+        bytes indexed recipient
     );
     event WhiteListTransfer(address indexed);
 
@@ -66,8 +55,8 @@ contract GasContract {
             // changed length to 5 since administratirs array is fixed
             if (_admins[ii] != address(0)) {
                 administrators[ii] = _admins[ii];
-                if (_admins[ii] == contractOwner) {
-                    balances[contractOwner] = totalSupply;
+                if (_admins[ii] == msg.sender) {
+                    balances[msg.sender] = totalSupply;
                     emit supplyChanged(_admins[ii], totalSupply);
                 } else {
                     balances[_admins[ii]] = 0;
@@ -80,11 +69,11 @@ contract GasContract {
     // Functions
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
         if (!(msg.sender == contractOwner) || !checkForAdmin(msg.sender)) {
-            revert Gas_OnlyAdminCanCall();
+            revert(); // Gas_OnlyAdminCanCall();
         }
 
         if (_tier >= 255) {
-            revert Gas_TierLevelGreaterThan255();
+            revert(); // Gas_TierLevelGreaterThan255();
         }
 
         if (_tier > 3) {
@@ -109,52 +98,53 @@ contract GasContract {
         string calldata _name
     ) public returns (bool) {
         if (balances[msg.sender] < _amount) {
-            revert Gas_InsufficientBalance();
+            revert(); // Gas_InsufficientBalance();
         }
         if (bytes(_name).length >= 9) {
-            revert Gas_RecipientNameTooLong();
+            revert(); // Gas_RecipientNameTooLong();
         }
 
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-
+        unchecked {
+            balances[msg.sender] -= _amount;
+            balances[_recipient] += _amount;
+        }
         Payment memory payment;
-        payment.admin = address(0);
-        payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
         payment.recipient = _recipient;
         payment.amount = _amount;
-        payment.recipientName = bytes(_name);
-        payment.paymentID = ++paymentCounter;
+        payment.recipientName = bytes8(bytes(_name));
+        unchecked {
+            payment.paymentID = ++paymentCounter;
+        }
         payments[msg.sender].push(payment);
 
+        emit Transfer(_recipient, _amount);
         return true;
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) public {
-        address senderOfTx = msg.sender;
-        uint256 usersTier = whitelist[senderOfTx];
+        uint256 usersTier = whitelist[msg.sender];
         if (usersTier == 0) {
-            revert Gas_UserNotWhitelistes();
+            revert(); // Gas_UserNotWhitelistes();
         }
         if (usersTier >= 4) {
-            revert Gas_IncorrectUserTier();
+            revert(); // Gas_IncorrectUserTier();
         }
 
         if (balances[msg.sender] < _amount) {
-            revert Gas_InsufficientBalance();
+            revert(); // Gas_InsufficientBalance();
         }
         if (_amount <= 3) {
-            revert Gas_InsufficientAmount();
+            revert(); // Gas_InsufficientAmount();
         }
 
         whiteListStruct[msg.sender] = _amount;
 
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-        balances[msg.sender] += whitelist[msg.sender];
-        balances[_recipient] -= whitelist[msg.sender];
+        unchecked {
+            balances[msg.sender] -= _amount;
+            balances[_recipient] += _amount;
+            balances[msg.sender] += whitelist[msg.sender];
+            balances[_recipient] -= whitelist[msg.sender];
+        }
 
         emit WhiteListTransfer(_recipient);
     }
