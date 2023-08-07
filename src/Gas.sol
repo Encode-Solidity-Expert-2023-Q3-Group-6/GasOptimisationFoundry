e73bf7f23f24e1502b5d5f6d96b5bafae487d133
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-08-08 02:14:57 +0530

    Update Changes

--- src/Gas.sol
+++ src/Gas.sol
@@ -3,33 +3,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-// error Gas_OnlyAdminCanCall();
-// error Gas_TierLevelGreaterThan255();
-// error Gas_UserNotWhitelistes();
-// error Gas_IncorrectUserTier();
-// error Gas_InsufficientBalance();
-// error Gas_RecipientNameTooLong();
-// error Gas_InsufficientAmount();
-
 contract GasContract {
-    // Type Decalarations
-
-    struct Payment {
-        uint256 paymentID;
-        uint256 amount;
-        address recipient;
-        bytes8 recipientName; // changed to bytes
-    } // Changed layout and type
-
     // State Variables
-    uint256 private immutable totalSupply; // Removed Initialization & changed to immutable
     uint256 private paymentCounter = 0;
-
+    address private immutable contractOwner;
     bool wasLastOdd = true;
-    address private contractOwner;
     address[5] public administrators;
 
-    mapping(address => Payment[]) private payments;
     mapping(address => uint256) public whitelist;
     mapping(address => uint256) public balances;
     mapping(address => bool) private isOddWhitelistUser;
@@ -37,30 +17,19 @@ contract GasContract {
 
     // Events
     event AddedToWhitelist(address userAddress, uint256 tier);
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address indexed recipient, uint256 indexed amount);
-    event PaymentUpdated(
-        address indexed admin,
-        uint256 indexed ID,
-        uint256 amount,
-        bytes indexed recipient
-    );
-    event WhiteListTransfer(address indexed);
+    event WhiteListTransfer(address indexed recipient);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
-        totalSupply = _totalSupply;
 
         for (uint256 ii = 0; ii < 5 /*administrators.length*/; ) {
             // changed length to 5 since administratirs array is fixed
             if (_admins[ii] != address(0)) {
                 administrators[ii] = _admins[ii];
                 if (_admins[ii] == msg.sender) {
-                    balances[msg.sender] = totalSupply;
-                    emit supplyChanged(_admins[ii], totalSupply);
+                    balances[msg.sender] = _totalSupply;
                 } else {
                     balances[_admins[ii]] = 0;
-                    emit supplyChanged(_admins[ii], 0);
                 }
             }
             unchecked {
@@ -71,7 +40,7 @@ contract GasContract {
 
     // Functions
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
-        if (!(msg.sender == contractOwner) || !checkForAdmin(msg.sender)) {
+        if (msg.sender != contractOwner || !checkForAdmin(msg.sender)) {
             revert(); // Gas_OnlyAdminCanCall();
         }
 
@@ -111,16 +80,6 @@ contract GasContract {
             balances[msg.sender] -= _amount;
             balances[_recipient] += _amount;
         }
-        Payment memory payment;
-        payment.recipient = _recipient;
-        payment.amount = _amount;
-        payment.recipientName = bytes8(bytes(_name));
-        unchecked {
-            payment.paymentID = ++paymentCounter;
-        }
-        payments[msg.sender].push(payment);
-
-        emit Transfer(_recipient, _amount);
         return true;
     }
 
